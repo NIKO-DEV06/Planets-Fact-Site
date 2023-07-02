0e5e26d5958b470eff7bfb63f9324157d58e8083
Ayeniko Emmanuel <ayenikoemmanuel06@gmail.com>
2023-07-02 23:11:21 +0100

    added nav

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,8 @@
 import { Antonio } from "next/font/google";
 import Nav from "./Nav";
+import menu from "@/assets/icon-hamburger.svg";
+import Image from "next/image";
+
 const spartan = Antonio({
   weight: ["200", "300", "400", "500", "600", "700"],
   subsets: ["latin"],
@@ -7,12 +10,18 @@ const spartan = Antonio({
 
 const Header = () => {
   return (
-    <header className="fixed w-screen">
-      <h1></h1>
-      <div>
-        <Nav />
-      </div>
-    </header>
+    <>
+      <header className="fixed w-screen flex flex-col justify-between items-center px-[1.5rem]">
+        <h1 className={`${spartan.className} text-[2.2rem] font-semibold`}>
+          THE PLANETS
+        </h1>
+        <div>
+          <Nav />
+          <Image src={menu} alt="hamburger" className="md:hidden w-[2rem]" />
+        </div>
+      </header>
+      <hr className="border-[0.5px] opacity-50 border-[#979797] absolute top-[4.5rem] w-full " />
+    </>
   );
 };
 
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,5 +1,34 @@
+import Link from "next/link";
+
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <nav className="hidden md:flex gap-[2rem] opacity-70">
+      <Link href={"/mercury"} className="uppercase">
+        mercury
+      </Link>
+      <Link href={"/venus"} className="uppercase">
+        venus
+      </Link>
+      <Link href={"/earth"} className="uppercase">
+        earth
+      </Link>
+      <Link href={"/mars"} className="uppercase">
+        mars
+      </Link>
+      <Link href={"/jupiter"} className="uppercase">
+        jupiter
+      </Link>
+      <Link href={"/saturn"} className="uppercase">
+        saturn
+      </Link>
+      <Link href={"/uranus"} className="uppercase">
+        uranus
+      </Link>
+      <Link href={"/neptune"} className="uppercase">
+        neptune
+      </Link>
+    </nav>
+  );
 };
 
 export default Nav;
